91f2831db65211cf8e39851d5d8dd588dbb4e6db
Domas <domas.grigaliunas.dev@gmail.com>
2022-03-24 16:44:17 +0000

    aded missing code for cratedAt column

--- confl/python/TweetCounts.py
+++ confl/python/TweetCounts.py
@@ -1,6 +1,7 @@
 
 class TweetCounts:
-    def __init__(self, id, retweetCount, favouriteCount):
+    def __init__(self, id, retweetCount, favouriteCount, createdAt):
         self.id = id
         self.retweetCount = retweetCount
         self.favouriteCount = favouriteCount
+        self.createdAt = createdAt
